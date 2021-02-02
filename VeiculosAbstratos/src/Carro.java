881a49c65a4b3ce665c7bd66a7f2acb0aaf0301a
professorisidro <isidro@professorisidro.com.br>
2021-02-02 14:15:39 -0300

    classes abstratas

---
+++ VeiculosAbstratos/src/Carro.java
@@ -0,0 +1,27 @@
+
+public class Carro extends Veiculo {
+	private String modelo;
+
+	public Carro(String marca, int ano, String modelo) {
+		super(marca, ano);
+		this.modelo = modelo;
+	}
+
+	@Override
+	public void acelerar() {
+		System.out.println("Carro "+super.marca+ "/"+this.modelo+ " acelera no pé VRUUUMMMM!");
+		
+	}
+
+	@Override
+	public void frear() {
+		System.out.println("Carro "+super.marca+"/"+this.modelo+" freia na mão IIIICCCCHHHHH");
+		
+	}
+
+	
+	
+	
+	
+
+}
